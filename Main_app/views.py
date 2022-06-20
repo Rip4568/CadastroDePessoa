f67d4f803fdfa180ec69ae2da8727014ce81ab92
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2022-06-19 23:52:03 -0300

    fist commit

---
+++ Main_app/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'Main_app/index.html'
